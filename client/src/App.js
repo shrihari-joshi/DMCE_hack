b0d2de21109fca023adb900ed40bb85225ae2353
AK-47Code <atharvakhabale2004@gmail.com>
2024-03-21 12:23:00 +0530

    Login and registration design

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import LobbyScreen from "./screens/Lobby";
 import RoomPage from "./screens/Room";
-
+import LoginForm from "./Component/Login/login";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<LobbyScreen />} />
+        {/* <Route path="/" element={<LobbyScreen />} /> */}
+        <Route path="/" element={<LoginForm />} />
         <Route path="/room/:roomId" element={<RoomPage />} />
       </Routes>
     </div>
